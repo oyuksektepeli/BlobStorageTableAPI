2c82f14c6a93ab0c5c39aa9401aff5a2eaab8f7c
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-03-23 13:09:12 +0300

    Add project files.

---
+++ BlobStorageTableAPI/MovieEntity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlobStorageTableAPI
+{
+	public class MovieEntity : TableEntity
+	{
+		public MovieEntity(string genre, string movieTitle)
+		{
+			base.PartitionKey = genre;
+			base.RowKey = movieTitle;
+		}
+
+		public MovieEntity() { }
+
+		public string Genre => base.PartitionKey;
+
+		public string Title => base.RowKey;
+
+		public int Year { get; set; }
+
+		public string Length { get; set; }
+
+		public string Description { get; set; }
+
+		public static explicit operator MovieEntity(TableResult v)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ BlobStorageTableAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlobStorageTableAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TableDemo.Run();
+        }
+    }
+}
